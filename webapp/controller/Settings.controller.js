a9447baf3ee7e8db5577dfd569373346eb40c403
Liat Borenshtein <liat.borenshtein@sap.com>
2016-11-29 17:32:45 +0000

    Add binding to the settings view

--- webapp/controller/Settings.controller.js
+++ webapp/controller/Settings.controller.js
@@ -1,18 +1,41 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function(Controller) {
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/model/json/JSONModel"
+], function(Controller, JSONModel) {
 	"use strict";
 
 	return Controller.extend("sign.controller.Settings", {
+
+		onChange: function(oEvent){
+			var view = this.getView();
+			var oModel = view.getModel();
+			var item = 	oEvent.getParameters().selectedItem;
+			var key = item.getKey();
+			if(key === "grid"){
+				oModel.setData({enabled: true}, true); 
+			}else{
+			oModel.setData({enabled: false}, true); 
+			}
+		},
 
 		/**
 		 * Called when a controller is instantiated and its View controls (if available) are already created.
 		 * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
 		 * @memberOf sign.view.Settings
 		 */
-		//	onInit: function() {
-		//
-		//	},
+			onInit: function() {
+				var view = this.getView();
+				var oModel = new JSONModel({
+					layout: [
+							{key:"grid", value:"טבלה"},//TBD get from i18n
+							{key:"scroll",value:"גלילה"}
+							],
+					categoriesNum: [{key: "1"}, {key: "2"}, {key:"3"}, {key:"4"}, {key:"5"}, {key:"6"}, {key:"7"}, {key:"8"}, {key:"9"}],
+					enabled: true
+				});
+				view.setModel(oModel); //TBD:Set name to the model
+		
+			}
 
 		/**
 		 * Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
