854223e0a8761d25ecfbe40b27f21ace1659eaf2
Liat Borenshtein <liat.borenshtein@sap.com>
2016-12-04 14:25:15 +0000

    Initial addition of an empty MasterGrid view and navigation to it

--- webapp/controller/MasterGrid.controller.js
+++ webapp/controller/MasterGrid.controller.js
@@ -1,14 +1,14 @@
 sap.ui.define([
-	"sign/controller/BaseController"
-], function(BaseController) {
+	"sap/ui/core/mvc/Controller"
+], function(Controller) {
 	"use strict";
 
-	return BaseController.extend("sign.controller.Welcome", {
+	return Controller.extend("sign.controller.MasterGrid", {
 
 		/**
 		 * Called when a controller is instantiated and its View controls (if available) are already created.
 		 * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
-		 * @memberOf sign.view.view.Welcome
+		 * @memberOf sign.view.view.MasterGrid
 		 */
 		//	onInit: function() {
 		//
@@ -17,7 +17,7 @@ sap.ui.define([
 		/**
 		 * Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
 		 * (NOT before the first rendering! onInit() is used for that one!).
-		 * @memberOf sign.view.view.Welcome
+		 * @memberOf sign.view.view.MasterGrid
 		 */
 		//	onBeforeRendering: function() {
 		//
@@ -26,7 +26,7 @@ sap.ui.define([
 		/**
 		 * Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
 		 * This hook is the same one that SAPUI5 controls get after being rendered.
-		 * @memberOf sign.view.view.Welcome
+		 * @memberOf sign.view.view.MasterGrid
 		 */
 		//	onAfterRendering: function() {
 		//
@@ -34,21 +34,11 @@ sap.ui.define([
 
 		/**
 		 * Called when the Controller is destroyed. Use this one to free resources and finalize activities.
-		 * @memberOf sign.view.view.Welcome
+		 * @memberOf sign.view.view.MasterGrid
 		 */
 		//	onExit: function() {
 		//
 		//	}
-
-		onPressPlay: function() {
-
-			this.getRouter().navTo("mastergrid");
-		},
-
-		onPressSettings: function() {
-
-			this.getRouter().navTo("settings");
-		}
 
 	});
 
