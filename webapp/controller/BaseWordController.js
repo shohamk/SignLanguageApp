580cd81aa8c04381303cb629d96fc6e17b6b7226
Liat Borenshtein <liat.borenshtein@sap.com>
2016-12-07 15:55:57 +0000

    Seperate Word view to two - WordGrid and Word. Create a common BaseWordController

---
+++ webapp/controller/BaseWordController.js
@@ -0,0 +1,51 @@
+sap.ui.define([
+	"sign/controller/BaseController",
+	"sap/ui/core/routing/History"
+], function(BaseController, History) {
+	"use strict";
+
+	return BaseController.extend("sign.controller.BaseWord", {
+
+		setContent: function(viewId) {
+			var oView = this.getView().byId(viewId);
+			var html1 = new sap.ui.core.HTML("html1", {
+				content: "<video width='100%' height='100%' autoplay controls>" +
+					"<source src='c.mp4' type='video/mp4'>" +
+					"Your browser does not support the video tag." +
+					"</video>"
+			});
+
+			oView.addContent(html1);
+		},
+
+		assignRoutePattern: function(sId, viewId) {
+			var that = this;
+			var oView = this.getView().byId(viewId);
+			var sPath = "/words('" + sId + "')";
+			var oModel = oView.getModel("sign");
+			var oData = oModel.getData(sPath);
+			oView.bindElement(sPath);
+			var video = $("#html1");
+			video.attr("src", oData.words[sId].video);
+
+			//if there is no data the model has to request new data
+			if (!oData) {
+				oView.getElementBinding().attachEventOnce("dataReceived", function() {
+					that._checkIfProductAvailable(sPath, sId);
+				});
+			}
+		},
+
+		handleNavigation: function(defaultRouteName) {
+			var oHistory, sPreviousHash;
+			oHistory = History.getInstance();
+			sPreviousHash = oHistory.getPreviousHash();
+			if (sPreviousHash !== undefined) {
+				window.history.go(-1);
+			} else {
+				this.getRouter().navTo(defaultRouteName, {}, true /*no history*/ );
+			}
+		}
+	});
+
+});
--- webapp/controller/WordGrid.controller.js
+++ webapp/controller/WordGrid.controller.js
@@ -3,26 +3,31 @@ sap.ui.define([
 ], function(BaseWordController) {
 	"use strict";
 
-	return BaseWordController.extend("sign.controller.Word", {
+	return BaseWordController.extend("sign.controller.WordGrid", {
 
+		/**
+		 * Called when a controller is instantiated and its View controls (if available) are already created.
+		 * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
+		 * @memberOf sign.view.WordGrid
+		 */
 		onInit: function() {
-			this.getRouter().getRoute("word").attachPatternMatched(this._routePatternMatched, this);
-			this.setContent("wordPage");
+			this.getRouter().getRoute("wordgrid").attachPatternMatched(this._routePatternMatched, this);
+			this.setContent("wordGridPage");
 		},
 
 		_routePatternMatched: function(oEvent) {
 			var sId = oEvent.getParameter("arguments").wordId;
-			this.assignRoutePattern(sId, "wordPage");
+			this.assignRoutePattern(sId, "wordGridPage");
 		},
 
 		handleNavButtonPress: function(oEvent) {
-			this.handleNavigation("master");
+			this.handleNavigation("mastergrid");
 		}
 
 		/**
 		 * Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
 		 * (NOT before the first rendering! onInit() is used for that one!).
-		 * @memberOf sign.view.Word
+		 * @memberOf sign.view.WordGrid
 		 */
 		//	onBeforeRendering: function() {
 		//
@@ -31,15 +36,15 @@ sap.ui.define([
 		/**
 		 * Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
 		 * This hook is the same one that SAPUI5 controls get after being rendered.
-		 * @memberOf sign.view.Word
+		 * @memberOf sign.view.WordGrid
 		 */
-		//onAfterRendering: function() {
-		// 
-		//}
+		//	onAfterRendering: function() {
+		//
+		//	},
 
 		/**
 		 * Called when the Controller is destroyed. Use this one to free resources and finalize activities.
-		 * @memberOf sign.view.Word
+		 * @memberOf sign.view.WordGrid
 		 */
 		//	onExit: function() {
 		//
